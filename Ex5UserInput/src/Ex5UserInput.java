3e510e94c0e3ccf023f287d5320c8db97b8b2737
Amalamol Raju <33655795+araju55@users.noreply.github.com>
2018-02-07 21:01:12 +0000

    Add files via upload

---
+++ Ex5UserInput/src/Ex5UserInput.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 17011947
+ */
+public class Ex5UserInput {
+
+    /**
+     * @param args the command line arguments
+     */import java.util.Scanner;
+
+/**
+ *
+ * @author 17011947
+ */
+public class Ex5UserInput {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        
+        System.out.print("Enter Width: ");
+        int width = kb.nextInt();
+        
+        System.out.print("Enter Height: ");
+        int height = kb.nextInt();
+        
+        int area = width * height;
+        System.out.println("Area is: " + area);
+    }
+}
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        
+        System.out.print("Enter Width: ");
+        int width = kb.nextInt();
+        
+        System.out.print("Enter Height: ");
+        int height = kb.nextInt();
+        
+        int area = width * height;
+        System.out.println("Area is: " + area);
+    }
+}
